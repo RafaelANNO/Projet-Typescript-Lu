de0871cb8c27498555a0509fb5df335681dcee42
RafaelANNO <anoj846@mail.com>
2020-12-05 20:28:01 +0100

    tous est en ts

--- dist/main.js
+++ dist/main.js
@@ -103,3 +103,101 @@ function create_the_table_from_json() {
     }
 }
 create_the_table_from_json();
+let all_option = [{
+        text: "Bambou 1",
+        stock: "instock",
+        price: 72,
+        tag: "Fitness",
+        image_src: "./assets/images/product_1.jpg"
+    }, {
+        text: "Bambou 2",
+        stock: "instock",
+        price: 56,
+        tag: "Fitness",
+        image_src: "./assets/images/product_2.jpg"
+    }, {
+        text: "Bambou 3",
+        stock: "instock",
+        price: 69,
+        tag: "Fitness",
+        image_src: "./assets/images/product_3.jpg"
+    }];
+let all_option_right = [];
+let leftside_table = document.getElementById("leftside_table");
+let select_leftside = document.createElement("select");
+select_leftside.setAttribute("id", "SelectList");
+select_leftside.setAttribute("multiple", "multiple");
+select_leftside.setAttribute("style", "width: 400px; height: 250px");
+leftside_table === null || leftside_table === void 0 ? void 0 : leftside_table.append(select_leftside);
+let rightside_table = document.getElementById("rightside_table");
+let select_rightside = document.createElement("select");
+select_rightside.setAttribute("id", "PickList");
+select_rightside.setAttribute("multiple", "multiple");
+select_rightside.setAttribute("style", "width: 400px; height: 250px");
+rightside_table === null || rightside_table === void 0 ? void 0 : rightside_table.append(select_rightside);
+initiateDisplay();
+function initiateDisplay() {
+    select_leftside.options.length = 0;
+    select_rightside.options.length = 0;
+    all_option.map(function (option, index) {
+        let item = new Option("", index.toString());
+        item.setAttribute("class", "the_option_of_each_product");
+        let image = document.createElement("img");
+        image.setAttribute("src", option.image_src);
+        let divContainer = document.createElement("div");
+        divContainer.setAttribute("class", "product_item_div");
+        let span1 = document.createElement("span");
+        span1.setAttribute("class", "text_of_the_product_span");
+        span1.innerHTML = option.text;
+        let span2 = document.createElement("span");
+        span2.setAttribute("class", "tag_of_the_product_span");
+        span2.innerHTML = option.tag;
+        let span3 = document.createElement("span");
+        span3.setAttribute("class", "stock_status_of_the_product_span");
+        span3.innerHTML = option.stock;
+        divContainer.append(span1);
+        divContainer.append(span2);
+        divContainer.append(span3);
+        divContainer.append(image);
+        item.append(divContainer);
+        select_leftside.append(item);
+    });
+    all_option_right.map(function (option, index) {
+        let item = new Option("", index.toString());
+        item.setAttribute("class", "the_option_of_each_product");
+        let divContainer = document.createElement("div");
+        divContainer.setAttribute("class", "product_item_div");
+        let span1 = document.createElement("span");
+        span1.setAttribute("class", "text_of_the_product_span");
+        span1.innerHTML = option.text;
+        let span2 = document.createElement("span");
+        span2.setAttribute("class", "tag_of_the_product_span");
+        span2.innerHTML = option.tag;
+        let span3 = document.createElement("span");
+        span3.setAttribute("class", "stock_status_of_the_product_span");
+        span3.innerHTML = option.stock;
+        divContainer.append(span1);
+        divContainer.append(span2);
+        divContainer.append(span3);
+        item.append(divContainer);
+        select_rightside.append(item);
+    });
+}
+function addIt() {
+    console.log('all_option :>> ', select_leftside.selectedIndex);
+    if (select_leftside.selectedIndex !== -1 && select_leftside.selectedIndex !== undefined) {
+        all_option_right.push(all_option[select_leftside.selectedIndex]);
+        all_option.splice(select_leftside.selectedIndex, 1);
+        initiateDisplay();
+    }
+    console.log('all_option :>> ', all_option);
+    console.log('all_option_right :>> ', all_option_right);
+}
+function delIt() {
+    if (select_rightside.selectedIndex !== -1 && select_rightside.selectedIndex !== undefined) {
+        all_option.push(all_option_right[select_rightside.selectedIndex]);
+        all_option_right.splice(select_rightside.selectedIndex, 1);
+        initiateDisplay();
+    }
+    console.log('all_option_right :>> ', all_option_right);
+}
